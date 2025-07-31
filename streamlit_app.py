64d9dfec9174882cb7e1af726cce3d2dee671295
George Merticariu <george.merticariu@snowflake.com>
2025-07-31 17:13:25 +0200

    remove edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Git Edit Welcome to Streamlit!
+# Welcome to Streamlit!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
